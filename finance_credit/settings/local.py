7c5df1f51d186d02aa2401230ab0b76e17430690
MegaSoftVision <45824438+MegaSoftVision@users.noreply.github.com>
2021-03-23 14:01:31 -0500

    Add files via upload

---
+++ finance_credit/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finance_maria', #your databases
+        'USER': 'yoel', #your user
+        'PASSWORD': 'ACYM23..',#your pass
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
